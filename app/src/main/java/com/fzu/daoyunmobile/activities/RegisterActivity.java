acfe3f0581006e6ca250b4a8f5d770c9661cbd88
ChenJG <dlvguo@qq.com>
2021-03-22 22:57:19 +0800

    up

--- app/src/main/java/com/fzu/daoyunmobile/activities/RegisterActivity.java
+++ app/src/main/java/com/fzu/daoyunmobile/activities/RegisterActivity.java
@@ -1,21 +1,66 @@
 package com.fzu.daoyunmobile.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.fzu.daoyunmobile.R;
 
-public class RegisterActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class RegisterActivity extends AppCompatActivity {
+    //验证码按钮
     private Button veriCodeBtn;
+    //注册按钮
+    private Button registerBtn;
+
+    //账号
+    private EditText userName;
+    //密码
+    private EditText password;
+    //确认密码
+    private EditText confPassword;
+    //验证码
+    private EditText veriCode;
+
+    //生成的验证码
+    private int verificationCode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        veriCodeBtn=(Button)findViewById(R.id.bt_veri_submit);
+        veriCodeBtn = (Button) findViewById(R.id.bt_veri_submit);
+        userName = findViewById(R.id.et_login_username);
+        password = findViewById(R.id.et_login_pwd);
+        confPassword = findViewById(R.id.et_reg_conf_pwd);
+        veriCode = findViewById(R.id.et_reg_vericode);
+        registerBtn = (Button) findViewById(R.id.bt_register_submit);
 
+        veriCodeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random r = new Random();
+                verificationCode = r.nextInt(899999) + 100000;
+                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this)
+                        .setTitle("验证码")
+                        .setMessage("验证码为：" + verificationCode)
+                        .setPositiveButton("确定", null);
+                builder.show();
+                veriCodeBtn.setText("已发送");
+                veriCodeBtn.setEnabled(false);
+            }
+        });
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
