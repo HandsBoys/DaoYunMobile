936a901d0cbb6794b3f7a8fe53cf65c91c15a572
ChenJG <dlvguo@qq.com>
2021-03-22 21:55:15 +0800

    up

--- app/src/main/java/com/fzu/daoyunmobile/activities/LoginActivity.java
+++ app/src/main/java/com/fzu/daoyunmobile/activities/LoginActivity.java
@@ -2,16 +2,30 @@ package com.fzu.daoyunmobile.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.fzu.daoyunmobile.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        registerBtn = (Button) findViewById(R.id.bt_login_register);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JumpToRegister();
+            }
+        });
+    }
 
+    private void JumpToRegister() {
+        startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
     }
 }
--- app/src/main/java/com/fzu/daoyunmobile/activities/RegisterActivity.java
+++ app/src/main/java/com/fzu/daoyunmobile/activities/RegisterActivity.java
@@ -3,14 +3,19 @@ package com.fzu.daoyunmobile.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.fzu.daoyunmobile.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button veriCodeBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        veriCodeBtn=(Button)findViewById(R.id.bt_veri_submit);
+
     }
 }
