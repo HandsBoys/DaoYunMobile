eb83ae4f27d0e8287b04b85c8cbb8b2e436d1853
ChenJG <dlvguo@qq.com>
2021-03-31 13:11:38 +0800

    Update LoginActivity.java

--- app/src/main/java/com/fzu/daoyunmobile/activities/LoginActivity.java
+++ app/src/main/java/com/fzu/daoyunmobile/activities/LoginActivity.java
@@ -21,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tab_test);
+        setContentView(R.layout.activity_login);
         initView();
     }
 
